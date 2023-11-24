c45be7a41f9e395ead6b786f85693655bdae44c3
Lyzix <maksim.v1liev@gmail.com>
2023-11-24 15:48:33 +0300

    xd 2

--- main.py
+++ main.py
@@ -1,15 +1,48 @@
 import sys
+
+from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QGraphicsScene, QGraphicsView, QGraphicsEllipseItem
-from PyQt5.QtCore import Qt
-from PyQt5.uic import loadUi
 from random import randint
 from PyQt5.QtGui import QColor, QBrush, QPainter
 
 
-class MyWindow(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(400, 300)
+        self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.pushButton = QtWidgets.QPushButton(parent=self.centralwidget)
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout.addWidget(self.pushButton)
+        self.graphicsView = QtWidgets.QGraphicsView(parent=self.centralwidget)
+        self.graphicsView.setObjectName("graphicsView")
+        self.verticalLayout.addWidget(self.graphicsView)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(parent=MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 400, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(parent=MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        self.pushButton.clicked.connect(MainWindow.add_circle)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Заспавнить окружности"))
+
+
+class MyWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MyWindow, self).__init__()
-        loadUi('UI.ui', self)
+        self.setupUi(self)
 
         self.pushButton.clicked.connect(self.add_circle)
 
@@ -21,7 +54,7 @@ class MyWindow(QMainWindow):
 
         for i in range(2):
             diameter = randint(20, 70)
-            color = QColor(255, 255, 0)
+            color = QColor(randint(0, 255), randint(0, 255), randint(0, 255))
             brush = QBrush(color)
 
             x = randint(0, self.graphicsView.width() - diameter)
